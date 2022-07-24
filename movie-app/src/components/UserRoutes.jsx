7516b2d81b09de3d2927d8bae29954b810aa43aa
testler <joshgarst@me.com>
2022-07-24 15:07:00 -0500

    fixed confilcts

---
+++ movie-app/src/components/UserRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowerRouter as Router, Route, Routes} from "react-router-dom";
+
+const UserRoutes = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UserRoutes
