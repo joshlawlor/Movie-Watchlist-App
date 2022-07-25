3b88461d03bcf4d2ea8185d4834708260a2b6c38
testler <joshgarst@me.com>
2022-07-25 00:51:46 -0500

    rebased to current code

--- movie-app/src/components/SignUpForm/SignUpForm.jsx
+++ movie-app/src/components/SignUpForm/SignUpForm.jsx
@@ -1,28 +1,45 @@
 import React, {useState} from 'react'
+import { Navigate } from 'react-router-dom';
 
-const SignUpForm = () => {
+const SignUpForm = ({backendURL}) => {
     const [userCred, SetUserCred] = useState({email: "", password: "", confirmPassword: ""})
+    const [errorCode, setErrorCode] = useState(0); // 1 is passoword don't match, 2 is email already taken
 
     function handleChange(event){
         SetUserCred({ ...userCred, [event.target.id]: event.target.value });
     };
+    async function testUserCred(){
+      await fetch(`${backendURL}/users/signup`,{method: "POST", header: userCred})
+      .then((response) => {
+          if(!response.ok){
+            console.log(response);
+          }else if(response === "email in use"){
+            setErrorCode(2);
+          }else{
+            setErrorCode(0);
+            <Navigate to="/HomePage" replace={true} /> // redirect to homepage
+          }
+      })
+   }
     function handleSubmit(e){
-        e.preventDefault();
-        if(userCred.password === userCred.confirmPassword){
-             //send a request to check the database here
-        }
-       else{
-            //send a message to user
-       }
+      e.preventDefault();
+      if(userCred.password === userCred.confirmPassword){
+        testUserCred();
+      }
+      else{
+        setErrorCode(1);
+      }
     }
   return (
     <form class='form' onSubmit={handleSubmit}>
       <br/>
         <label htmlFor="Email">Email</label>
         <input onChange={handleChange} type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input onChange={handleChange} type="password" name="password" id="password" />
-        <label htmlFor="password">Confirm Password</label>
+        {(errorCode===2)?<p className='error'>this email is already associated with an account</p>:null}
+        <label htmlFor="password">password</label>
+        <input onChange={handleChange} type="password" name="password" id="password"/>
+        {(errorCode===1)?<p className='error'>passwords don't match</p>:null}
+        <label htmlFor="password">confirm password</label>
         <input onChange={handleChange} type="confirmPassword" name="confirmPassword" id="confirmPassword" />
         <br/>
         <button type="submit">Sign Up</button>
