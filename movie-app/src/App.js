9902f14583ff36a0154fd5aec1b4ff9e1b29ba4a
testler <joshgarst@me.com>
2022-07-28 19:48:59 -0500

    created framework and landingPage

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -46,5 +46,18 @@ function App(){
     </div>
   )
 }
+import React, { Component } from 'react';
+import './App.css';
+import MovieRoutes from './components/MovieRoutes';
+import UserRoutes from './components/UserRoutes';
+import LandingPage from './pages/LandingPage';
+
+function App(){
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
 
 export default App;
